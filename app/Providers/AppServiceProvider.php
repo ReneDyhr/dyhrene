5f115ec7c14fd23c1bd46b9074e2df9f1cd0077e
René Dyhr <bazzo39@gmail.com>
2025-06-08 17:14:47 +0000

    feat(url): ✨ add macros for alternate signature validation
    * Implemented `alternateHasCorrectSignature` and `alternateHasValidSignature` macros in `UrlGenerator`.
    * Added `hasValidSignature` macro to the `Request` class for easier signature validation.
    * Enhances security by ensuring URL signatures are correctly validated.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,7 +14,35 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void {}
+    public function register(): void
+    {
+        UrlGenerator::macro(
+            'alternateHasCorrectSignature',
+            function (Request $request, bool $absolute = true, array $ignoreQuery = []): bool {
+                $ignoreQuery[] = 'signature';
+
+                $absoluteUrl = \url($request->path());
+                $url = $absolute ? $absoluteUrl : '/' . $request->path();
+
+                $queryString = \collect(\explode('&', $request
+                    ->server->getString('QUERY_STRING')))
+                    ->reject(fn(string $parameter) => \in_array(\Str::before($parameter, '='), $ignoreQuery))
+                    ->join('&');
+
+                $original = \rtrim($url . '?' . $queryString, '?');
+                $signature = \hash_hmac('sha256', $original, \Config::string('app.key'));
+
+                return \hash_equals($signature, (string) $request->string('signature', ''));
+            },
+        );
+        UrlGenerator::macro('alternateHasValidSignature', function (Request $request, bool $absolute = true, array $ignoreQuery = []): bool {
+            return URL::alternateHasCorrectSignature($request, $absolute, $ignoreQuery)
+                && URL::signatureHasNotExpired($request);
+        });
+        Request::macro('hasValidSignature', function (bool $absolute = true, array $ignoreQuery = []): mixed {
+            return URL::alternateHasValidSignature($this, $absolute, $ignoreQuery);
+        });
+    }
 
     /**
      * Bootstrap any application services.
