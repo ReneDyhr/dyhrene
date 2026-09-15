592c5b79f251cb11c9ae8e432efa391645400e39
René Dyhr <bazzo39@gmail.com>
2026-09-15 03:38:21 +0000

    migrate recipe create/edit to the dark design
    - Replace the .new-recipe/.col-6 wrapper with view-head + card
    - Restyle fields with form-group/form-control/btn; keep multiSelect, tagsinput
      and sortable IDs + JS intact
    - Remove the leftover alert demo divs (Information/Success/Warning/Error)
    - Danish labels and popover help text

--- resources/views/livewire/recipes/add.blade.php
+++ resources/views/livewire/recipes/add.blade.php
@@ -1,185 +1,150 @@
 @section('title', $title)
 <x-layouts.app-shell :area="\App\Enums\AppArea::Kitchen">
-        <div class="content">
-            <div class="col-12">
-                <div class="new-recipe">
-                    <div class="col-6">
-                        <form id="form" wire:submit.prevent="save">
-                            <h1>
-                                <input type="text" tabindex="1" wire:model="name"
-                                    class="title noEnterSubmit"
-                                    placeholder="Title">
-                            </h1>
-                            <div class="ingredients-list form-group">
-                                <label for="arrtibutevalue">
-                                    Add Ingredient
-                                    <i class="fa fa-info-circle" title="Help" data-toggle="popover"
-                                    data-placement="top"
-                                    data-content="Type one ingredient at a time, with the amount and press the tick.<br>The ingredients will be listed from first to last added ingredient.<br>If there's a mistake or an ingredient need a correction, add the corrected ingredient to the list, move it using the arrows, and delete the old one pressing the cross.<br><b>Start with a # and it will be a headline</b><br><span style='font-size:0.7rem;'>eg. 50 g chopped nuts, of choice</span>">
-                                    </i>
-                                </label>
-                                <div class="attline">
-                                    <div class="attval">
-                                        <input tabindex="2" type="text" wire:model="newIngredient" wire:keydown.enter="addIngredient" id="arrtibutevalue"
-                                            class="noEnterSubmit" value="">
-                                    </div>
-                                    <div class="attvaladdl" wire:ignore>
-                                        <a href="javascript:void(0);" wire:click="addIngredient" id="attlink">
-                                            <i class="fa fa-check"></i>
-                                        </a>
-                                    </div>
-                                    <div class="clear"></div>
-                                </div>
-                            </div>
+    <div class="view-head">
+        <h1>{{ $title }}</h1>
+        <p>Udfyld opskriften og tilføj ingredienser.</p>
+    </div>
 
-                            <div class="ingredients-list form-group">
-                                <label>Ingredients List</label>
-                                <ul class="ingredients" id="ingredients">
-                                    @foreach ($ingredients as $index => $ingredient)
-                                        <li wire:key="ingredient-{{ $index }}">
-                                            <input type="hidden" wire:model="ingredients.{{ $index }}" value="{{$ingredient}}">
-                                            <input disabled="disabled" class="attributevalue" value="{{$ingredient}}">
-                                            <a href="#">
-                                                <i class="fa fa-arrows-v"></i>
-                                            </a>
-                                            <a href="javascript:void(0);" wire:click="removeIngredient({{ $loop->index }})">
-                                                <i class="fa fa-times"></i>
-                                            </a>
-                                        </li>
-                                    @endforeach
-                                </ul>
-                                <div class="clear"></div>
-                            </div>
-                            <div class="form-group">
-                                <label for="description">
-                                    Description
-                                    <i class="fa fa-info-circle" title="Help" data-toggle="popover"
-                                    data-placement="top"
-                                    data-content="Describe how to make the food.<br><span style='font-size:0.7rem;'>eg. Whip the eggs before slowly adding them to the mixture.<br>Baked for 20 mins. at 180 degrees.<br>Make sure the cake has been in the oven for at least 20 mins before opening. It can collapse!</span>">
-                                    </i>
-                                </label>
-                                <textarea
-                                    tabindex="3"
-                                    rows="10"
-                                    id="description"
-                                    wire:model="description"
-                                    class="text-field form-control">
-                                </textarea>
-                            </div>
-                            <div class="form-group">
-                                <label for="note">Note
-                                    <i class="fa fa-info-circle" title="Help" data-toggle="popover"
-                                    data-placement="top"
-                                    data-content="Note things you want to remember with this recipe.<br><span style='font-size:0.7rem;'>eg. Works well, served with sour cream.<br>Martin enjoys this very much.</span>">
-                                    </i>
-                                </label>
-                                <textarea tabindex="4" rows="10" id="note" wire:model="note"
-                                        class="text-field form-control"></textarea>
-                            </div>
-                            <div class="category-list form-group" wire:ignore>
-                                <label for="selectCategories">Categories<i class="fa fa-info-circle" title="Help" data-toggle="popover"
-                                    data-placement="top"
-                                    data-content="Choose one or more categories for the recipe. This will help finding the recipe again, even if you forget the title you give it."></i>
-                                </label>
-                                <select tabindex="5" multiple="multiple" id="selectCategories" wire:model="categories"
-                                        class="form-control" style="height:200px;"
-                                        data-usesprite="smallIcons">
-                                    @foreach (\App\Models\Category::with('icon')->forAuthUser()->get() as $index => $category)
-                                        <option wire:key="category-{{ $index }}" value="{{$category->id}}"
-                                            data-icon="{{$category->icon->class}}"
-                                        >{{$category->name}}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="tags form-group" wire:ignore>
-                                <label for="tags">Tags
-                                    <i class="fa fa-info-circle" title="Help" data-toggle="popover"
-                                    data-placement="top"
-                                    data-content="Adding tags to a recipe, can make it easier to find it again. It can be ingredients, allergy-prone ingredients or the appropriate celebration, in which it's served.<br><span style='font-size:0.7rem;'>eg. christmas, nuts, gluten, chocolate</span>"></i>
-                                </label>
-                                <input data-tab="6" type="text" id="tags" wire:model="tags" class="tags-input">
-                            </div>
-                            <div class="form-check" style="margin-bottom:10px;">
-                                <label class="form-check-label">
-                                    <input  tabindex="7" name="public"
-                                        value="1" type="checkbox" wire:model="public"
-                                        class="form-check-input"> Should this be public?</label>
-                            </div>
-                            <div class="form-group">
-                                <input tabindex="8" id="submit" type="submit" name="edit"
-                                    class="noEnterSubmit btn btn-success" value="Save Changes">
-                            </div>
-                            <div class="clear"></div>
-                        </form>
-                    </div>
-                    <div class="clear"></div>
+    <form id="form" wire:submit.prevent="save" class="card">
+        <div class="form-group">
+            <label for="name">Titel</label>
+            <input type="text" tabindex="1" wire:model="name" id="name" class="form-control noEnterSubmit" placeholder="Titel">
+        </div>
+
+        <div class="form-group">
+            <label for="arrtibutevalue">
+                Tilføj ingrediens
+                <i class="fa fa-info-circle" title="Hjælp" data-toggle="popover" data-placement="top"
+                    data-content="Skriv én ingrediens ad gangen med mængde, og tryk på fluebenet.<br>Ingredienserne vises i den rækkefølge de tilføjes.<br><b>Start med # og det bliver en overskrift.</b><br><span style='font-size:0.7rem;'>fx 50 g hakkede nødder</span>"></i>
+            </label>
+            <div class="attline" style="display: flex; gap: 10px;">
+                <input tabindex="2" type="text" wire:model="newIngredient" wire:keydown.enter="addIngredient" id="arrtibutevalue" class="form-control noEnterSubmit" style="flex: 1;">
+                <div wire:ignore>
+                    <a href="javascript:void(0);" wire:click="addIngredient" id="attlink" class="btn btn-success" style="margin-top: 0;"><i class="fa fa-check"></i></a>
                 </div>
             </div>
-            @script
-            <script>
-                let initialized = false;
-                $(document).ready(function () {
-                    $('.noEnterSubmit').keypress(function (e) {
-                        if (e.which == 13 && !$("#submit").is(":focus"))
-                            e.preventDefault();
-                    });
-                    $('#selectCategories').multiSelect({
-                        keepOrder: true,
-                        selectableHeader: "<div style='text-align:center;font-weight:bold;font-size:12px;'>Not Choosen</div>",
-                        selectionHeader: "<div style='text-align:center;font-weight:bold;font-size:12px;'>Choosen</div>",
-                    });
-
-                    $('[data-toggle="popover"]').popover({
-                        html: true
-                    });
-                    $('#attlink').hide();
-        
-                    $('#arrtibutevalue').keydown(function (e) {
-                        if (e.keyCode == 13) {
-                            $('#attlink').click();
-                        }
-                    });
-                    $('#arrtibutevalue').keyup(function (event) {
-                        if ($('#arrtibutevalue').val().length > 0) {
-                            $('#attlink').fadeIn();
-                        }
-                        if ($('#arrtibutevalue').val().length === 0) {
-                            $('#attlink').fadeOut();
-                        }
-                    });
-
-                    $('#tags').tagsinput();
-                    $('#tags').attr('wire:model', 'tags');
-                    $('#tags').on('itemAdded', function(event) {
-                        @this.set('tags', $(this).val());
-                    });
-                });
-
-                $('#selectCategories').on('change', function() {
-                    @this.set('categories', $(this).val());
-                });
-        
-                $('#ingredients').sortable(
-                    {
-                        update: function(event, ui) {
-                            let ingredients = [];
-                            $('#ingredients li').each(function() {
-                                ingredients.push($(this).find('input').val());
-                            });
-                            @this.set('ingredients', ingredients);
-                        }
-                    }
-                );
-            </script>
-            @endscript
         </div>
-            <script>
-                $(function () {
-                    $('[data-toggle="tooltip"]').tooltip()
-                })
-            </script>
-            <div class="alert alert-info" role="alert"><header>Information</header><main><span class="alert_text"></span></main></div>
-        <div class="alert alert-success" role="alert"><header>Success</header><main><span class="alert_text"></span></main></div>
-        <div class="alert alert-warning" role="alert"><header>Warning</header><main><span class="alert_text"></span></main></div>
-        <div class="alert alert-danger" role="alert"><header>Error</header><main><span class="alert_text"></span></main></div>
+
+        <div class="form-group">
+            <label>Ingredienser</label>
+            <ul class="ingredients" id="ingredients" style="list-style: none; margin: 0; padding: 0;">
+                @foreach ($ingredients as $index => $ingredient)
+                    <li wire:key="ingredient-{{ $index }}" style="display: flex; align-items: center; gap: 10px; padding: 6px 0; border-bottom: 1px solid var(--line);">
+                        <input type="hidden" wire:model="ingredients.{{ $index }}" value="{{ $ingredient }}">
+                        <input disabled="disabled" class="attributevalue" value="{{ $ingredient }}" style="flex: 1; background: transparent; border: 0; color: var(--ink);">
+                        <a href="#" style="cursor: grab; color: var(--ink-soft);"><i class="fa fa-arrows-v"></i></a>
+                        <a href="javascript:void(0);" wire:click="removeIngredient({{ $loop->index }})" style="color: #e0533f;"><i class="fa fa-times"></i></a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+
+        <div class="form-group">
+            <label for="description">
+                Beskrivelse
+                <i class="fa fa-info-circle" title="Hjælp" data-toggle="popover" data-placement="top"
+                    data-content="Beskriv hvordan maden laves.<br><span style='font-size:0.7rem;'>fx Pisk æggene inden de langsomt tilsættes blandingen.</span>"></i>
+            </label>
+            <textarea tabindex="3" rows="10" id="description" wire:model="description" class="form-control"></textarea>
+        </div>
+
+        <div class="form-group">
+            <label for="note">
+                Note
+                <i class="fa fa-info-circle" title="Hjælp" data-toggle="popover" data-placement="top"
+                    data-content="Notér ting du vil huske om opskriften.<br><span style='font-size:0.7rem;'>fx Smager godt med creme fraiche.</span>"></i>
+            </label>
+            <textarea tabindex="4" rows="10" id="note" wire:model="note" class="form-control"></textarea>
+        </div>
+
+        <div class="form-group" wire:ignore>
+            <label for="selectCategories">
+                Kategorier
+                <i class="fa fa-info-circle" title="Hjælp" data-toggle="popover" data-placement="top"
+                    data-content="Vælg én eller flere kategorier for opskriften. Det gør det nemmere at finde den igen."></i>
+            </label>
+            <select tabindex="5" multiple="multiple" id="selectCategories" wire:model="categories" class="form-control" style="height: 200px;" data-usesprite="smallIcons">
+                @foreach (\App\Models\Category::with('icon')->forAuthUser()->get() as $index => $category)
+                    <option wire:key="category-{{ $index }}" value="{{ $category->id }}" data-icon="{{ $category->icon->class }}">{{ $category->name }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="form-group" wire:ignore>
+            <label for="tags">
+                Tags
+                <i class="fa fa-info-circle" title="Hjælp" data-toggle="popover" data-placement="top"
+                    data-content="Tags gør det nemmere at finde opskriften igen. Det kan være ingredienser, allergener eller anledningen den serveres til."></i>
+            </label>
+            <input data-tab="6" type="text" id="tags" wire:model="tags" class="tags-input form-control">
+        </div>
+
+        <div class="form-group">
+            <label style="display: flex; align-items: center; gap: 8px; font-weight: normal;">
+                <input tabindex="7" name="public" value="1" type="checkbox" wire:model="public"> Skal opskriften være offentlig?
+            </label>
+        </div>
+
+        <div style="display: flex; gap: 10px; margin-top: 20px;">
+            <input tabindex="8" id="submit" type="submit" name="edit" class="btn btn-success noEnterSubmit" value="Gem">
+            <a href="{{ route('recipes.index') }}" class="btn btn-default" wire:navigate>Annuller</a>
+        </div>
+    </form>
+
+    @script
+    <script>
+        $(document).ready(function () {
+            $('.noEnterSubmit').keypress(function (e) {
+                if (e.which == 13 && !$("#submit").is(":focus"))
+                    e.preventDefault();
+            });
+            $('#selectCategories').multiSelect({
+                keepOrder: true,
+                selectableHeader: "<div style='text-align:center;font-weight:bold;font-size:12px;'>Ikke valgt</div>",
+                selectionHeader: "<div style='text-align:center;font-weight:bold;font-size:12px;'>Valgt</div>",
+            });
+
+            $('[data-toggle="popover"]').popover({
+                html: true
+            });
+            $('#attlink').hide();
+
+            $('#arrtibutevalue').keydown(function (e) {
+                if (e.keyCode == 13) {
+                    $('#attlink').click();
+                }
+            });
+            $('#arrtibutevalue').keyup(function (event) {
+                if ($('#arrtibutevalue').val().length > 0) {
+                    $('#attlink').fadeIn();
+                }
+                if ($('#arrtibutevalue').val().length === 0) {
+                    $('#attlink').fadeOut();
+                }
+            });
+
+            $('#tags').tagsinput();
+            $('#tags').attr('wire:model', 'tags');
+            $('#tags').on('itemAdded', function(event) {
+                @this.set('tags', $(this).val());
+            });
+        });
+
+        $('#selectCategories').on('change', function() {
+            @this.set('categories', $(this).val());
+        });
+
+        $('#ingredients').sortable(
+            {
+                update: function(event, ui) {
+                    let ingredients = [];
+                    $('#ingredients li').each(function() {
+                        ingredients.push($(this).find('input').val());
+                    });
+                    @this.set('ingredients', ingredients);
+                }
+            }
+        );
+    </script>
+    @endscript
 </x-layouts.app-shell>
